62562f7aca321b81f988b68c4997b11a830f51a5
Pau Urós Cortés <pau.uros@campus.xaloc.org>
2025-09-12 16:02:44 +0200

    Add files via upload
    Segunda prueba que hicimos en programación

---
+++ Prueba 2.py
@@ -0,0 +1,11 @@
+var1=3
+var2=3.4
+var3=98
+total=var1+var2
+print(f"el total de {var1} + {var2} es: ", total)
+var1=int(input("introduce un numero: "))
+var2=float(input("introduce un numero: "))
+var3=float(input("introduce un numero: "))
+total=var1+var2+var3
+print("el resultado es", total)
+
