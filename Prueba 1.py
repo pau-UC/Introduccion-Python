e6f59741c80d9b91ad0dd00eb1820edc19b76f44
pau-UC <pau.uros@campus.xaloc.org>
2025-09-12 15:42:44 +0200

    Add files via upload
    Esta fue mi primer día de programación

---
+++ Prueba 1.py
@@ -0,0 +1,14 @@
+print("hola buenos")
+#variable numërica
+var1=4
+#variable decimal
+var2=4.5
+#variable texto
+var3="hola"
+
+var_4="adios"
+
+print("el valor es", var1)
+print("el valor es", var2)
+print("el valor es", var3)
+print("el valor es", var_4)
