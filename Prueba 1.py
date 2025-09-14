d309a01bb7bdb58b9af86b262d5aa3334fe216c1
Pau Urós Cortés <pau.uros@campus.xaloc.org>
2025-09-14 18:21:36 +0200

    Prueba 1.py

--- Prueba 1.py
+++ Prueba 1.py
@@ -1,10 +1,14 @@
-var1=3
-var2=3.4
-var3=98
-total=var1+var2
-print(f"el total de {var1} + {var2} es: ", total)
-var1=int(input("introduce un numero: "))
-var2=float(input("introduce un numero: "))
-var3=float(input("introduce un numero: "))
-total=var1+var2+var3
-print("el resultado es", total)
+print("hola buenos")
+#variable numërica
+var1=4
+#variable decimal
+var2=4.5
+#variable texto
+var3="hola"
+var_4="adios"
+print("el valor es", var1)
+print("el valor es", var2)
+print("el valor es", var3)
+print("el valor es", var_4)
+
+
