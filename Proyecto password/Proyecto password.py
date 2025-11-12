11ac6da4949261fe4fd3228696ade9fb5430d3a5
Pau Urós Cortés <pau.uros@campus.xaloc.org>
2025-11-12 09:55:47 +0100

    Update Proyecto password.py

--- Proyecto password/Proyecto password.py
+++ Proyecto password/Proyecto password.py
@@ -12,54 +12,61 @@ print("     Posición 7 uno de los siguientes símbolos &, /, # ")
 print("     Posición 8 un número menor o igual que 5")
 password=input("introduce una palabra clave: ")
 comprobador=True
+corrector=0
 if len(password) >=6 and len(password) <=8:
     if password[0].isnumeric() and int(password[0])>=1 and int(password[0])<=5:
         print("")
     else:
+        comprobador=False
         print("Error en el caracter 1")
-        comprobador==False
     if password[1].islower():
         print("")
     else:
+        comprobador=False
         print("Error en el caracter 2")
-        comprobador==False
     if password[2].isupper():
         print("")
     else:
+        comprobador=False
         print("Error en el caracter 3")
-        comprobador==False
-    if password[3]== "*" or "_" or "@":
+    if password[3] in ["*" or "_" or "@"]:
         print("")
     else:
+        comprobador=False
         print("Error en el caracter 4")
-        comprobador==False
     if password[4].islower():
         print("")
     else:
+        comprobador=False
         print("Error en el caracter 5")
-        comprobador==False
     if password[5].isnumeric and int(password[5])>=6 and int(password[5])<=9:
         if len(password)==6 and comprobador==True:
             print("El format del PASSWORD és correcte")
+            corrector=1
         else:
             print("")
     else:
+        comprobador=False
         print("Error en el caracter 6")
-        Comprobador=False
-    if len(password)==7 and password[6]=="&" or "/" or "#":
+    if len(password)==7 and password[6] in ["&" or "/" or "#"]:
         if len(password)==7 and comprobador==True:
             print("El format del PASSWORD és correcte")
+            corrector==1
         else:
             print("")
+    elif corrector==1:
+        print("")
     else:
+        comprobador=False
         print("Error en el caracter 7")
-    if len(password)==8 and password[7].isnumeric() and int(password[7])<=5:
-        if len(password)==8 and comprobador==True:
-            print("El format del PASSWORD és correcte")
-        else:
-            print("")
+    if len(password)==8 and password[7].isnumeric() and int(password[7])<=5 and comprobador==True:
+        print("El format del PASSWORD és correcte")
+    elif corrector==1:
+        print("")
+    elif len(password)==8 and password[7].isnumeric() and int(password[7])<=5:
+        print("")
     else:
+        comprobador=False
         print("Error en el caracter 8")       
 else:
-    longitud=len(password)
-    print("Error, el password tiene una longitud de", longitud, "caracteres i no cumple los requisitos")
+    print("Error, el password tiene una longitud de", len(password), "caracteres i no cumple los requisitos")
