da30ae3c2b3fcdef0003b009b03209e7d611a8db
Pau Urós Cortés <pau.uros@campus.xaloc.org>
2026-01-28 09:56:08 +0100

    Update Ejercicio DNI.py

--- Ejercicio DNI.py/Ejercicio DNI.py
+++ Ejercicio DNI.py/Ejercicio DNI.py
@@ -1,11 +1,17 @@
 
 dni=input("Introduce los números del DNI: ")
 resto=0
+Lista_intentos=[]
 letras=["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"]
 if len(dni)==8:
-    resto=int(dni)%23
-    for i in range(len(letras)):
-        if letras[i]==letras[resto]:
-            dni+= "-" + letras[i]
-print(dni)
-print(resto)
+    if dni.isnumeric():
+        resto=int(dni)%23
+        for i in range(len(letras)):
+            if letras[i]==letras[resto]:
+                dni+= "-" + letras[i]
+        print(dni)
+        print(resto)
+    else:
+            print("El valor introducido debe ser numerico")
+else:
+    print("El valor introducido no cumple con la longitud correcta")
