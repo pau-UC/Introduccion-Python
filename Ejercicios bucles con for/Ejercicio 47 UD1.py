a55474214dc75d924d51a9396ca198b682b2fea4
Pau Urós Cortés <pau.uros@campus.xaloc.org>
2026-02-18 09:39:21 +0100

    python

--- Ejercicios bucles con for/Ejercicio 47 UD1.py
+++ Ejercicios bucles con for/Ejercicio 47 UD1.py
@@ -1,31 +1,14 @@
 # Realiza un programa donde el usuario introduzca por teclado 2 intervalos, por pantalla se debe mostrar el rango de números teniendo en cuenta que se a<b la secuencia será incremental y si a>b la secuencia en descenso.
 numero1=int(input("introduce el primer intervalo: "))
 numero2=int(input("introduce el segundo intervalo: "))
-<<<<<<< Updated upstream
-if numero1>numero2:
-    print(numero1,"-")
-    repeticiones=numero1-numero2
-    for i in range(repeticiones):
-        numero1-=1
-        print(numero1,"-")
-elif numero2>numero1:
-    print(numero2,"-")
-    repeticiones=numero2-numero1
-    for i in range(repeticiones):
-        numero2-=1
-        print(numero2,"-")
-
-
-=======
 numero = ""
 if numero1>numero2:
     repeticiones=numero1-numero2
     for i in range(repeticiones):
-        numero = numero1 + str(i) + "-"
+        numero+= numero1 + str(i) + "-"
     print(numero)
 elif numero1<numero2:
     repeticiones=numero2-numero1
     for i in range(repeticiones):
-        numero= numero1 + str(i) + "-"
+        numero+= numero1 + str(i) + "-"
     print(numero)
->>>>>>> Stashed changes
