f1f7b6ba2e05ebfc8040aa9cb4bd06a5def20e8f
Pau Urós Cortés <pau.uros@campus.xaloc.org>
2026-02-18 09:40:27 +0100

    python

--- Ejercicios bucles con for/Ejercicio 45 UD1.py
+++ Ejercicios bucles con for/Ejercicio 45 UD1.py
@@ -1,4 +1,3 @@
-
 #  Realiza un programa que permita introducir una palabra por teclado y puedas recorrer el string distinguiendo vocales y las consonantes: 
 palabra=input("introduce una palabra: ")
 vocales=""
--- Ejercicios bucles con for/Ejercicio 47 UD1.py
+++ Ejercicios bucles con for/Ejercicio 47 UD1.py
@@ -1,14 +1,15 @@
 # Realiza un programa donde el usuario introduzca por teclado 2 intervalos, por pantalla se debe mostrar el rango de números teniendo en cuenta que se a<b la secuencia será incremental y si a>b la secuencia en descenso.
 numero1=int(input("introduce el primer intervalo: "))
 numero2=int(input("introduce el segundo intervalo: "))
-numero = ""
 if numero1>numero2:
+    print(numero1,"-")
     repeticiones=numero1-numero2
     for i in range(repeticiones):
-        numero+= numero1 + str(i) + "-"
-    print(numero)
-elif numero1<numero2:
+        numero1-=1
+        print(numero1,"-")
+elif numero2>numero1:
+    print(numero2,"-")
     repeticiones=numero2-numero1
     for i in range(repeticiones):
-        numero+= numero1 + str(i) + "-"
-    print(numero)
+        numero2-=1
+        print(numero2,"-")
