cce0589ab3f945631f04ee913f5238f4cac8521e
Pau Urós Cortés <pau.uros@campus.xaloc.org>
2026-02-20 16:06:25 +0100

    Update Proyecto ahorcado Pau Urós.py

--- Proyecto ahorcado/Proyecto ahorcado Pau Urós.py
+++ Proyecto ahorcado/Proyecto ahorcado Pau Urós.py
@@ -1,16 +1,24 @@
 import random
 lista_palabrasecreta=["patata","tomate","esternoclidomastoideo","marmota","aeropuerto","libreria","submarino","sublime","alpha","controversia"]
 lista_partida=[]
-lista_ahorcado=["AHORCADO"]
+lista_ahorcado=["A","H","O","R","C","A","D","O"]
 longitud="_"
 repetir="s"
+error=0
+errores=0
 aleatorio=random.choice(lista_palabrasecreta)
 for i in range(len(aleatorio)):
     lista_partida+=longitud.split(",")
 while repetir!="n":
-    print(lista_partida)
-    letra=input("Introduce una letra: ")
-    for i in range(len(aleatorio)):
-        if letra==aleatorio[i]:
-            lista_partida[i]=letra
+    while error!=8:
+        print(lista_partida)
+        letra=input("Introduce una letra: ")
+        for i in range(len(aleatorio)):
+            if letra==aleatorio[i]:
+                lista_partida[i]=letra
+            else:
+                error+=1
+                for i in range(len(lista_ahorcado)):
+                    errores+=lista_ahorcado[error]
+                    print(lista_ahorcado)
     repetir=input("deseas realizar otro ahorcado s/n: ")
